dee34f7ecd75db26d5ad205e1c93daec8c13f92e
FirenLF2 <cheekyhagrid@gmail.com>
2024-05-05 19:50:47 +0200

    setting up router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
 })
 export class AppComponent {
   title = 'filterComm';
