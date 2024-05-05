a6fecafcaec798efe0b4e86efa3b47370e400139
FirenLF2 <cheekyhagrid@gmail.com>
2024-05-05 20:31:39 +0200

    ngFor filters

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FiltersService } from '../services/filters/filters.service';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  filters: String[] = [];
+  constructor(private filterService: FiltersService) {}
 
+  ngOnInit(): void {
+    this.filters = this.filterService.getAll();
+  }
 }
