f05d5b52350ebeeb4ace33a9818c61e4b93852bf
FirenLF2 <cheekyhagrid@gmail.com>
2024-05-05 20:12:10 +0200

    images to array conversion

--- src/app/services/filters/filters.service.ts
+++ src/app/services/filters/filters.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FiltersService {
+  constructor() {}
 
-  constructor() { }
+  getAll(): String[] {
+    return [
+      '/assets/images/filters/filter-1.jpg',
+      '/assets/images/filters/filter-2.jpg',
+      '/assets/images/filters/filter-3.jpg',
+      '/assets/images/filters/filter-4.jpeg',
+      '/assets/images/filters/filter-5.jpg',
+      '/assets/images/filters/filter-6.jpg',
+    ];
+  }
 }
