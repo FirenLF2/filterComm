fc34b5f183b7048e9646d104f8cc4db1c508cc5b
FirenLF2 <cheekyhagrid@gmail.com>
2024-05-07 17:02:10 +0200

    Implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FiltersService } from '../services/filters/filters.service';
 import { CommonModule } from '@angular/common';
 import { filter } from 'rxjs';
+import { Filter } from '../shared/models/filters';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@ import { filter } from 'rxjs';
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-  filters: String[] = [];
+  filters: Filter[] = [];
   constructor(private filterService: FiltersService) {}
 
   ngOnInit(): void {
--- src/app/services/filters/filters.service.ts
+++ src/app/services/filters/filters.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Filter } from '../../shared/models/filters';
 
 @Injectable({
   providedIn: 'root',
@@ -6,14 +7,74 @@ import { Injectable } from '@angular/core';
 export class FiltersService {
   constructor() {}
 
-  getAll(): String[] {
+  getAll(): Filter[] {
     return [
-      '/assets/images/filters/filter-1.jpg',
-      '/assets/images/filters/filter-2.jpg',
-      '/assets/images/filters/filter-3.jpg',
-      '/assets/images/filters/filter-4.jpeg',
-      '/assets/images/filters/filter-5.jpg',
-      '/assets/images/filters/filter-6.jpg',
+      {
+        id: 1,
+        name: 'Pizza Pepperoni',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        stars: 4.5,
+        imageUrl: '/assets/images/filters/filter-1.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
+      {
+        id: 2,
+        name: 'Meatball',
+        price: 20,
+        cookTime: '20-30',
+        favorite: true,
+        origins: ['persia', 'middle east', 'china'],
+        stars: 4.7,
+        imageUrl: '/assets/images/filters/filter-2.jpg',
+        tags: ['SlowFood', 'Lunch'],
+      },
+      {
+        id: 3,
+        name: 'Hamburger',
+        price: 5,
+        cookTime: '10-15',
+        favorite: false,
+        origins: ['germany', 'us'],
+        stars: 3.5,
+        imageUrl: '/assets/images/filters/filter-3.jpg',
+        tags: ['FastFood', 'Hamburger'],
+      },
+      {
+        id: 4,
+        name: 'Fried Potatoes',
+        price: 2,
+        cookTime: '15-20',
+        favorite: true,
+        origins: ['belgium', 'france'],
+        stars: 3.3,
+        imageUrl: '/assets/images/filters/filter-4.jpeg',
+        tags: ['FastFood', 'Fry'],
+      },
+      {
+        id: 5,
+        name: 'Chicken Soup',
+        price: 11,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['india', 'asia'],
+        stars: 3.0,
+        imageUrl: '/assets/images/filters/filter-5.jpg',
+        tags: ['SlowFood', 'Soup'],
+      },
+      {
+        id: 6,
+        name: 'Vegetables Pizza',
+        price: 9,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['italy'],
+        stars: 4.0,
+        imageUrl: '/assets/images/filters/filter-6.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
     ];
   }
 }
