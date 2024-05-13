91ed3479d5e7c2503a5555619a087e942a856294
FirenLF2 <cheekyhagrid@gmail.com>
2024-05-13 13:26:45 +0200

    HomeComp change

--- src/app/home/HomeComponent.ts
+++ src/app/home/HomeComponent.ts
@@ -1,33 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { FiltersService } from '../services/filters/filters.service';
-import { CommonModule } from '@angular/common';
+import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
 import { Filter } from '../shared/models/filters';
-import { ActivatedRoute } from '@angular/router';
-import { filter } from 'rxjs';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgOptimizedImage, CurrencyPipe, SearchComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
+  searchTerm: String | null = '';
   filters: Filter[] = [];
+
   constructor(
     private filterService: FiltersService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      if (params.searchTerm)
+    this.route.paramMap.subscribe((paramMap) => {
+      this.searchTerm = paramMap.get('searchTerm');
+      if (this.searchTerm) {
+        // @ts-ignore
         this.filters = this.filterService
           .getAll()
           .filter((filter) =>
-            filter.name.toLowerCase().includes(params.searchTerm.toLowerCase())
+            filter.name.toLowerCase().includes(this.searchTerm!.toLowerCase())
           );
-      else this.filters = this.filterService.getAll();
+      } else {
+        this.filters = this.filterService.getAll();
+      }
     });
   }
 }
