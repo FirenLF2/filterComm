6932fb94a84a3846b59d53d9118bf8d87556c080
FirenLF2 <cheekyhagrid@gmail.com>
2024-05-13 14:04:39 +0200

    Common Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/HomeComponent';
 
@@ -8,7 +9,7 @@ import { HomeComponent } from './home/HomeComponent';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, CommonModule],
 })
 export class AppComponent {
   title = 'filterComm';
