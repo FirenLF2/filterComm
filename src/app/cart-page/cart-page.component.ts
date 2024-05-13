c202431bacbe3a5977fc2e416b1ce4ff1439189c
FirenLF2 <cheekyhagrid@gmail.com>
2024-05-13 13:16:23 +0200

    big changes

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,39 +1,36 @@
 import { Component } from '@angular/core';
-import {CartService} from "../services/cart/cart.service";
-import {Cart} from "../share/models/cart";
-import {CartItem} from "../share/models/cartItem";
-import {RouterLink} from "@angular/router";
-import {CurrencyPipe} from "@angular/common";
+import { CartService } from '../services/cart/cart.service';
+import { Cart } from '../shared/models/cart';
+import { CartItem } from '../shared/models/cartItem';
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-page',
   standalone: true,
   imports: [RouterLink, CurrencyPipe],
   templateUrl: './cart-page.component.html',
-  styleUrl: './cart-page.component.css'
+  styleUrl: './cart-page.component.css',
 })
 export class CartPageComponent {
   cart!: Cart;
   constructor(private cartService: CartService) {
-    this.setCart()
+    this.setCart();
   }
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   removeFromCart(cartItem: CartItem) {
-    this.cartService.removeFromCart(cartItem.food.id);
+    this.cartService.removeFromCart(cartItem.filter.id);
     this.setCart();
   }
 
   changeQuantity(cartItem: CartItem, quantityInString: string) {
     const quantity = parseInt(quantityInString);
-    this.cartService.changeQuantity(cartItem.food.id, quantity);
+    this.cartService.changeQuantity(cartItem.filter.id, quantity);
     this.setCart();
   }
 
   setCart() {
     this.cart = this.cartService.getCart();
   }
-
 }
