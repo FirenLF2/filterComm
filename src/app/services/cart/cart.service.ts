c57feeb7e018a764e48d0a993a3ac0116334ba36
FirenLF2 <cheekyhagrid@gmail.com>
2024-05-15 15:32:12 +0200

    saveCart implementation

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -8,27 +8,44 @@ import { CartItem } from '../../shared/models/cartItem';
 })
 export class CartService {
   private cart: Cart = new Cart();
-  constructor() {}
+  private cartKey = 'cart';
+
+  constructor() {
+    this.cart = this.loadCart();
+  }
+
+  private saveCart() {
+    localStorage.setItem(this.cartKey, JSON.stringify(this.cart));
+  }
+
+  private loadCart(): Cart {
+    const cartJson = localStorage.getItem(this.cartKey);
+    return cartJson ? JSON.parse(cartJson) : { items: [] };
+  }
 
   addToCart(filter: Filter): void {
     let cartItem = this.cart.items.find((item) => item.filter.id === filter.id);
     if (cartItem) {
       this.changeQuantity(filter.id, cartItem.quantity + 1);
       return;
+    } else {
+      this.cart.items.push(new CartItem(filter));
     }
-    this.cart.items.push(new CartItem(filter));
+    this.saveCart();
   }
 
   removeFromCart(filterId: number): void {
     this.cart.items = this.cart.items.filter(
       (item) => item.filter.id !== filterId
     );
+    this.saveCart();
   }
 
   changeQuantity(filterId: number, quantity: number) {
     let cartItem = this.cart.items.find((item) => item.filter.id === filterId);
     if (!cartItem) return;
     cartItem.quantity = quantity;
+    this.saveCart();
   }
 
   getCart(): Cart {
